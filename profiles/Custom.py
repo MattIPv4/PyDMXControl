23df5781e51dce2cf8ea881cd2f98697bb48991a
MattIPv4 <matthew@cowley.org.uk>
2018-02-02 19:19:40 +0000

    custom fixture cause lol

---
+++ profiles/Custom.py
@@ -0,0 +1,12 @@
+from profiles.Fixture import Fixture
+
+
+class Custom(Fixture):
+
+    def __init__(self, *args, **kwargs):
+        chans = args[0]
+        args = args[1:]
+        super().__init__(*args, **kwargs)
+
+        for _ in range(chans):
+            self._register_channel(str(_+1))
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from controller import Controller
 from profiles.Generic import Generic
 from profiles.LED_RGB import LED_RGB
+from profiles.Custom import Custom
 
 # Create our controller
 dmx = Controller()
@@ -8,12 +9,16 @@ dmx = Controller()
 # Create some fixtures
 dimmer = dmx.add_fixture(Generic(dmx.next_channel))
 led = dmx.add_fixture(LED_RGB(dmx.next_channel))
+custom = dmx.add_fixture(Custom(5, dmx.next_channel))
 
 # Set dimmer
 dimmer.set_channel('dimmer', 255)
 
 # Set our RGB fixture
 led.set_channels(255, 128, 0)
+
+# Test custom
+custom.set_channel(1, 10)
 
 # View the DMX out
 print(dmx.channels)
