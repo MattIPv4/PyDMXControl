5b541eee97cedbe9c2b469dbfe6cc1f5b0354ae5
MattIPv4 <matthew@cowley.org.uk>
2018-02-02 21:53:29 +0000

    notes

--- test.py
+++ test.py
@@ -11,13 +11,13 @@ dimmer = dmx.add_fixture(Generic(dmx.next_channel))
 led = dmx.add_fixture(LED_RGB(dmx.next_channel))
 custom = dmx.add_fixture(Custom(5, dmx.next_channel))
 
-# Set dimmer
+# Set dimmer (1 chan)
 dimmer.set_channel('dimmer', 10)
 
-# Set our RGB fixture
+# Set our RGB fixture (3 chan + vdim)
 led.set_channels(255, 128, 0, 0)
 
-# Test custom
+# Test custom (5 chan, setting chan 1)
 custom.set_channel(1, 10)
 
 # View the DMX out
