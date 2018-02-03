0856bc024c974555ec75491cc5cee28632412e8c
MattIPv4 <matthew@cowley.org.uk>
2018-02-03 11:52:33 +0000

    prep for live test with correct fixtures

---
+++ profiles/Stairville/LED_Par_36.py
@@ -0,0 +1,13 @@
+from profiles.defaults.Vdim import Vdim
+
+
+class Stairville_LED_Par_36(Vdim):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._register_channel('mode')
+        self._register_channel('red', vdim=True)
+        self._register_channel('green', vdim=True)
+        self._register_channel('blue', vdim=True)
+        self._register_channel('speed')
