ff34cabbeed3e7e772cd47d613265c4237497f62
MattIPv4 <matthew@cowley.org.uk>
2018-10-20 15:07:00 +0100

    add correct exceptions and warnings in prototype

--- tests/json_config.py
+++ tests/json_config.py
@@ -8,62 +8,54 @@ def doLoad(file):
     from json import load, JSONDecodeError
     import re
     from importlib import import_module
+    from warnings import warn
+    from PyDMXControl.utils.exceptions import PyDMXControlException
+
+    class JSONConfigException(PyDMXControlException):
+
+        def __init__(self, filename: str, extra_info: str = ""):
+            super().__init__("Failed to load JSON file '{}'{}.".format(
+                filename, ", {}".format(extra_info) if extra_info else ""))
 
     try:
         with open(file) as f:
             data = load(f)
-    except (FileNotFoundError, OSError) as e:
-        # exception: bad file name
-        print("Failed to open JSON file '{}'".format(file))
-        print(e)
-        return
-    except JSONDecodeError as e:
-        # exception: bad file contents
-        print("Failed to load JSON file '{}'".format(file))
-        print(e)
-        return
+    except (FileNotFoundError, OSError):
+        raise JSONConfigException(file)
+    except JSONDecodeError:
+        raise JSONConfigException(file, "unable to parse contents")
 
     if not isinstance(data, list):
-        # exception: list of objects
-        print("Failed to load from JSON, expected list of objects, got {}".format(type(data)))
-        return
+        raise JSONConfigException(file, "expected list of dicts, got {}".format(type(data)))
 
     for index, item in enumerate(data):
         if not isinstance(item, dict):
-            # warn: expected dict
-            print("Failed to load item {} from JSON, expected dict, got {}".format(index, type(item)))
+            warn("Failed to load item {} from JSON, expected dict, got {}".format(index, type(item)))
             continue
 
         if 'type' not in item:
-            # warn: expected type property
-            print("Failed to load item {} from JSON, expected a type property".format(index))
+            warn("Failed to load item {} from JSON, expected a type property".format(index))
             continue
 
         pattern = re.compile(r"^(([\w\d.]+)\.)*([\w\d]+)$", re.IGNORECASE)
         match = pattern.match(item['type'])
         if not match:
-            # warn: couldn't understand type
-            print("Failed to load item {} from JSON, failed to parse type '{}'".format(index, item['type']))
+            warn("Failed to load item {} from JSON, failed to parse type '{}'".format(index, item['type']))
             continue
 
         try:
             module = import_module(".{}".format(match.group(2)), 'PyDMXControl.profiles')
-        except ModuleNotFoundError as e:
-            # warn: profile not found
-            print("Failed to load item {} from JSON, profile module '{}' not found".format(index, match.group(2)))
-            print(e)
+        except ModuleNotFoundError:
+            warn("Failed to load item {} from JSON, profile module '{}' not found".format(index, match.group(2)))
             continue
 
         try:
             module = getattr(module, match.group(3))
-        except AttributeError as e:
-            # warn: profile not found
-            print("Failed to load item {} from JSON, profile type '{}' not found in '{}'".format(index, match.group(3),
-                                                                                                 match.group(2)))
-            print(e)
+        except AttributeError:
+            warn("Failed to load item {} from JSON, profile type '{}' not found in '{}'".format(index, match.group(3),
+                                                                                                match.group(2)))
             continue
 
-        print(module)
         del item['type']
         dmx.add_fixture(module, **dict(item))
 
