95b7788026506945aac75138c853274bca56ff1e
MattIPv4 <matthew@cowley.org.uk>
2018-11-05 18:53:59 +0000

    add demo links in readme

--- tests/web.py
+++ tests/web.py
@@ -1,4 +1,5 @@
 from PyDMXControl.controllers import uDMXController as Controller
+from PyDMXControl.effects.Intensity import Dim
 from PyDMXControl.profiles.Eyourlife import Small_Flat_Par
 from PyDMXControl.profiles.Stairville import LED_Par_10mm, LED_Par_36
 
@@ -13,8 +14,28 @@ dmx.add_fixture(LED_Par_36)
 dmx.add_fixture(Small_Flat_Par)
 dmx.add_fixture(Small_Flat_Par)
 
+
+def strobe():
+    dmx.all_off()
+    Dim.group_apply(dmx.get_all_fixtures(), 50)
+
+
+def clear():
+    dmx.all_locate()
+    dmx.clear_all_effects()
+
+
 # Web Debug
-dmx.web_control()
+dmx.web_control(
+    callbacks={
+        "strobe": strobe,
+        "clear": clear
+    },
+    host="0.0.0.0",
+    port=80
+)
+
+# Web console is now accessible at http://0.0.0.0/ and will have two custom global callbacks for testing.
 
 # Dim all up
 dmx.all_locate()
