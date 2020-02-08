b12fa8dfa7824747e95a4a177f364637cc8b7066
Leander Claes <claes@emt.uni-paderborn.de>
2020-02-08 17:51:34 +0100

    Add profile for Showtec Compact PAR 7 Q4. (#35)
    * Add profile for Showtec Compact PAR 7 Q4.
    
    * Use separate classes for Showtec PAR depending on channel config.
    
    * Add generic class for Showtec Compact PAR.

---
+++ PyDMXControl/profiles/Showtec/_Compact_PAR.py
@@ -0,0 +1,94 @@
+"""
+ *  PyDMXControl: A Python 3 module to control DMX using uDMX.
+ *                Featuring fixture profiles, built-in effects and a web control panel.
+ *  <https://github.com/MattIPv4/PyDMXControl/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+"""
+
+from ..defaults import Fixture, Vdim
+
+
+class Compact_PAR_7_Q4_4Ch(Vdim):
+
+    def __init__(self, *args, **kwargs):
+        """
+        These models can be configured to use 4, 6, or 11 DMX channels. Use this
+        class for the 4 channel configuration.
+        """
+        super().__init__(*args, **kwargs)
+
+        self._register_channel('red', vdim=True)
+        self._register_channel_aliases('red', 'r')
+        self._register_channel('green', vdim=True)
+        self._register_channel_aliases('green', 'g')
+        self._register_channel('blue', vdim=True)
+        self._register_channel_aliases('blue', 'b')
+        self._register_channel('white', vdim=True)
+        self._register_channel_aliases('white', 'w')
+
+
+class Compact_PAR_7_Q4_6Ch(Fixture):
+
+    def __init__(self, *args, **kwargs):
+        """
+        These models can be configured to use 4, 6, or 11 DMX channels. Use this
+        class for the 6 channel configuration.
+        """
+        super().__init__(*args, **kwargs)
+
+        self._register_channel('dimmer')
+        self._register_channel_aliases('dimmer', 'dim', 'd')
+        self._register_channel('strobe')
+        self._register_channel('red')
+        self._register_channel_aliases('red', 'r')
+        self._register_channel('green')
+        self._register_channel_aliases('green', 'g')
+        self._register_channel('blue')
+        self._register_channel_aliases('blue', 'b')
+        self._register_channel('white')
+        self._register_channel_aliases('white', 'w')
+
+
+class Compact_PAR_7_Q4_11Ch(Fixture):
+
+    def __init__(self, *args, **kwargs):
+        """
+        These models can be configured to use 4, 6, or 11 DMX channels. Use this
+        class for the 11 channel configuration.
+        """
+        super().__init__(*args, **kwargs)
+
+        self._register_channel('dimmer')
+        self._register_channel_aliases('dimmer', 'dim', 'd')
+        self._register_channel('strobe')
+        self._register_channel('random strobe')
+        self._register_channel('color presets')
+        self._register_channel('color running')
+        self._register_channel('color running speed')
+        self._register_channel('sound mode')
+        self._register_channel('red')
+        self._register_channel_aliases('red', 'r')
+        self._register_channel('green')
+        self._register_channel_aliases('green', 'g')
+        self._register_channel('blue')
+        self._register_channel_aliases('blue', 'b')
+        self._register_channel('white')
+        self._register_channel_aliases('white', 'w')
+
+
+class Compact_PAR_7_Q4(Fixture):
+
+    def __init__(self, mode: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if mode == 4:
+            new = Compact_PAR_7_Q4_4Ch(*args, **kwargs)
+        elif mode == 6:
+            new = Compact_PAR_7_Q4_6Ch(*args, **kwargs)
+        elif mode == 11:
+            new = Compact_PAR_7_Q4_11Ch(*args, **kwargs)
+        else:
+            raise ValueError('Number of channels (mode) has to be 4, 6, or 11. You passed '
+                             '{}.'.format(mode))
+
+        self.__dict__ = new.__dict__
---
+++ PyDMXControl/profiles/Showtec/__init__.py
@@ -0,0 +1,25 @@
+"""
+ *  PyDMXControl: A Python 3 module to control DMX using uDMX.
+ *                Featuring fixture profiles, built-in effects and a web control panel.
+ *  <https://github.com/MattIPv4/PyDMXControl/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/MattIPv4/PyDMXControl/blob/master/LICENSE> or <http://www.gnu.org/licenses/>.
+"""
+
+from ._Compact_PAR import Compact_PAR_7_Q4_4Ch, Compact_PAR_7_Q4_6Ch, Compact_PAR_7_Q4_11Ch, \
+    Compact_PAR_7_Q4
+from ._Compact_PAR import Compact_PAR_7_Q4_4Ch as Showtec_Compact_PAR_7_Q4_4Ch
+from ._Compact_PAR import Compact_PAR_7_Q4_6Ch as Showtec_Compact_PAR_7_Q4_6Ch
+from ._Compact_PAR import Compact_PAR_7_Q4_11Ch as Showtec_Compact_PAR_7_Q4_11Ch
+from ._Compact_PAR import Compact_PAR_7_Q4 as Showtec_Compact_PAR_7_Q4
